67af2376d85391cc1b70c925f616e1b2fb39e797
Gopal <gopalroy5397@gmail.com>
2023-06-28 08:43:51 +0530

    made like accio

--- src/components/Name.js
+++ src/components/Name.js
@@ -3,14 +3,18 @@ import React,{useState} from "react";
 const Name =()=>{
     let [curr,setCurr] = useState("");
     function fun(event){
-        setCurr(event.target.value);
+        if (event.target.value==="") {
+            setCurr("");
+        }
+        else
+        setCurr("Hello "+ event.target.value+"!");
     }
     return (
         <div>
-            <P>Enter your name: </P>
-            <input onChange={fun} ></input>
-            <p>Hello {curr}! </p>
+            <p>Enter your name: </p>
+            <input onChange={fun} />
+            <p>{curr} </p>
         </div>
     )
 }
-export default Name;
+export default Name;  
